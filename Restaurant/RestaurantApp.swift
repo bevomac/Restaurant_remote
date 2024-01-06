dcfaa009e1c23f6c11f5f26ad2567930c73e0a3a
Bev McGhee <bmcghee@gmx.com>
2024-01-05 23:58:23 -0600

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Bev McGhee on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
