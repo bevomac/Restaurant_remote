13d6dfc84687ced9495ce57938044ccad975a810
Bev McGhee <bmcghee@gmx.com>
2024-01-06 02:05:06 -0600

    updated and completed the About view.

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct GallaryView: View {
+struct GalleryView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    GallaryView()
+    GalleryView()
 }
